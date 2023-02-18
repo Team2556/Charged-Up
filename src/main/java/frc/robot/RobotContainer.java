df5262b746b44a2adbdea5f71d92635f5d4ccdea
Nawa <43589616+NawaPlayz@users.noreply.github.com>
2023-02-18 09:07:56 -0600

    Swerve Code
    Functional swerve driving code.
    Turning needs to be fixed due to all of the wheels rotating inwards, this should be due to the angle wrapping being incorrect.
    Field-centric also needs to be tested once turning is fixed.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,17 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.SwerveDrive;
+import frc.robot.commands.TestCommand;
+import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.TestSubsystem;
+
+import java.util.Set;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,15 +23,28 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+  SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+
+  public static boolean fieldRelativeDriving = false;
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController xbox1 =
+      new CommandXboxController(Constants.OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    // Configure default commands
+
+    swerveSubsystem.setDefaultCommand(
+            // The left stick controls translation of the robot.
+            // Turning is controlled by the X axis of the right stick.
+            new SwerveDrive(
+                    swerveSubsystem,
+                    xbox1::getLeftY,
+                    xbox1::getLeftX,
+                    xbox1::getRightX,
+                    fieldRelativeDriving));
     // Configure the trigger bindings
     configureBindings();
   }
@@ -42,13 +59,7 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
   /**
@@ -58,6 +69,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return new WaitCommand(0);
   }
 }
---
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,166 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.sensors.CANCoder;
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.*;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Swerve.ModulePosition;
+import frc.robot.Constants.Swerve.Ports;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static frc.robot.Constants.Swerve.*;
+
+public class SwerveSubsystem extends SubsystemBase {
+    private final HashMap<ModulePosition, SwerveModule> m_swerveModules =
+            new HashMap<>(
+                    Map.of(
+                            ModulePosition.FRONT_LEFT,
+                            new SwerveModule(
+                                    0,
+                                    new CANSparkMax(Ports.frontLeftTurnMotor, CANSparkMaxLowLevel.MotorType.kBrushless),
+                                    new WPI_TalonFX(Ports.frontLeftDriveMotor),
+                                    new DutyCycleEncoder(Ports.frontLeftBoreEncoder),
+                                    frontLeftCANCoderOffset),
+                            ModulePosition.FRONT_RIGHT,
+                            new SwerveModule(
+                                    1,
+                                    new CANSparkMax(Ports.frontRightTurnMotor, CANSparkMaxLowLevel.MotorType.kBrushless),
+                                    invertMotor(Ports.frontRightDriveMotor),
+                                    new DutyCycleEncoder(Ports.frontRightBoreEncoder),
+                                    frontRightCANCoderOffset),
+                            ModulePosition.BACK_LEFT,
+                            new SwerveModule(
+                                    2,
+                                    new CANSparkMax(Ports.backLeftTurnMotor, CANSparkMaxLowLevel.MotorType.kBrushless),
+                                    invertMotor(Ports.backLeftDriveMotor),
+                                    new DutyCycleEncoder(Ports.backLeftBoreEncoder),
+                                    backLeftCANCoderOffset),
+                            ModulePosition.BACK_RIGHT,
+                            new SwerveModule(
+                                    3,
+                                    new CANSparkMax(Ports.backRightTurnMotor, CANSparkMaxLowLevel.MotorType.kBrushless),
+                                    invertMotor(Ports.backRightDriveMotor),
+                                    new DutyCycleEncoder(Ports.backRightBoreEncoder),
+                                    backRightCANCoderOffset)));
+    private final AHRS gyro = new AHRS();
+
+    private final SwerveDriveOdometry m_odometry =
+            new SwerveDriveOdometry(
+                    kSwerveKinematics,
+                    getHeadingRotation2d(),
+                    getModulePositions(),
+                    new Pose2d());
+
+    private ProfiledPIDController m_xController =
+            new ProfiledPIDController(kP_X, 0, kD_X, kThetaControllerConstraints);
+    private ProfiledPIDController m_yController =
+            new ProfiledPIDController(kP_Y, 0, kD_Y, kThetaControllerConstraints);
+    private ProfiledPIDController m_turnController =
+            new ProfiledPIDController(kP_Theta, 0, kD_Theta, kThetaControllerConstraints);
+
+    public SwerveSubsystem() {
+       gyro.reset();
+    }
+
+    public void drive(
+            double throttle,
+            double strafe,
+            double rotation,
+            boolean isFieldRelative,
+            boolean isOpenLoop) {
+        throttle *= kMaxSpeedMetersPerSecond;
+        strafe *= kMaxSpeedMetersPerSecond;
+        rotation *= kMaxRotationRadiansPerSecond;
+
+        ChassisSpeeds chassisSpeeds =
+                isFieldRelative
+                        ? ChassisSpeeds.fromFieldRelativeSpeeds(
+                        throttle, strafe, rotation, getHeadingRotation2d())
+                        : new ChassisSpeeds(throttle, strafe, rotation);
+
+        SwerveModuleState[] moduleStates = kSwerveKinematics.toSwerveModuleStates(chassisSpeeds);
+
+        SwerveDriveKinematics.desaturateWheelSpeeds(moduleStates, kMaxSpeedMetersPerSecond);
+
+        for (SwerveModule module : m_swerveModules.values())
+            module.setDesiredState(moduleStates[module.getModuleNumber()], isOpenLoop);
+    }
+
+    public void setSwerveModuleStates(SwerveModuleState[] states, boolean isOpenLoop) {
+        SwerveDriveKinematics.desaturateWheelSpeeds(states, kMaxSpeedMetersPerSecond);
+
+        for (SwerveModule module : m_swerveModules.values())
+            module.setDesiredState(states[module.getModuleNumber()], isOpenLoop);
+    }
+
+    public double getHeadingDegrees() {
+        return Math.IEEEremainder(gyro.getAngle(), 360);
+    }
+
+    public Rotation2d getHeadingRotation2d() {
+        return Rotation2d.fromDegrees(getHeadingDegrees());
+    }
+
+    public Pose2d getPoseMeters() {
+        return m_odometry.getPoseMeters();
+    }
+
+    public SwerveModule getSwerveModule(int moduleNumber) {
+        return m_swerveModules.get(ModulePosition.values()[moduleNumber]);
+    }
+
+    public SwerveModuleState[] getModuleStates() {
+        return new SwerveModuleState[] {
+                m_swerveModules.get(ModulePosition.FRONT_LEFT).getState(),
+                m_swerveModules.get(ModulePosition.FRONT_RIGHT).getState(),
+                m_swerveModules.get(ModulePosition.BACK_LEFT).getState(),
+                m_swerveModules.get(ModulePosition.BACK_RIGHT).getState()
+        };
+    }
+    public SwerveModulePosition[] getModulePositions() {
+        return new SwerveModulePosition[] {
+                m_swerveModules.get(ModulePosition.FRONT_LEFT).getPosition(),
+                m_swerveModules.get(ModulePosition.FRONT_RIGHT).getPosition(),
+                m_swerveModules.get(ModulePosition.BACK_LEFT).getPosition(),
+                m_swerveModules.get(ModulePosition.BACK_RIGHT).getPosition()
+        };
+    }
+
+    public void updateOdometry() {
+        m_odometry.update(getHeadingRotation2d(), getModulePositions());
+
+        for (SwerveModule module : m_swerveModules.values()) {
+            var modulePositionFromChassis =
+                    kModuleTranslations[module.getModuleNumber()]
+                            .rotateBy(getHeadingRotation2d())
+                            .plus(getPoseMeters().getTranslation());
+            module.setModulePose(
+                    new Pose2d(
+                            modulePositionFromChassis,
+                            module.getHeadingRotation2d().plus(getHeadingRotation2d())));
+        }
+    }
+
+    public WPI_TalonFX invertMotor(int port) {
+        WPI_TalonFX talonFX = new WPI_TalonFX(port);
+        talonFX.setInverted(true);
+        return talonFX;
+    }
+
+    private void updateSmartDashboard() {}
+
+    @Override
+    public void periodic() {
+        updateOdometry();
+        updateSmartDashboard();
+    }
+}
