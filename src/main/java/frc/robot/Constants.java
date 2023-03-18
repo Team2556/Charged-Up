836921d13d9f93a906c01b02d39d602c84b31dfa
Adamisbad <120685629+Adamisbad@users.noreply.github.com>
2023-03-18 13:11:58 -0500

    Intake
    Still untested, but when the intake is ready you can see if it works.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,16 +4,116 @@
 
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+
+    public static final int kDriverYAxis = 1;
+    public static final int kDriverXAxis = 0;
+    public static final int kDriverRotAxis = 4;
+    public static final int kDriverFieldOrientedButtonIdx = 1;
+
+    public static final double kDeadband = 0.05;
   }
+
+  public static final class Swerve {
+    public static final double kTrackWidth = Units.inchesToMeters(30);
+    public static final double kWheelBase = Units.inchesToMeters(30);
+
+    public static final Translation2d[] kModuleTranslations = {
+            new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+            new Translation2d(-kWheelBase / 2, -kTrackWidth / 2),
+            new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+            new Translation2d(kWheelBase / 2, -kTrackWidth / 2)
+    };
+
+    public static final double frontLeftCANCoderOffset  = 0.0;
+    public static final double frontRightCANCoderOffset = 0.0;
+    public static final double backLeftCANCoderOffset   = 0.0;
+    public static final double backRightCANCoderOffset  = 0.0;
+
+    public static final SwerveDriveKinematics kSwerveKinematics =
+            new SwerveDriveKinematics(kModuleTranslations);
+
+    public static final double kMaxSpeedMetersPerSecond = Units.feetToMeters(18);
+    public static final double kMaxRotationRadiansPerSecond = Math.PI * 2.0;
+    public static final double kMaxRotationRadiansPerSecondSquared = Math.PI * 2.0;
+
+    public static final double kP_X = 0.1;
+    public static final double kD_X = 0;
+    public static final double kP_Y = 0.1;
+    public static final double kD_Y = 0;
+    public static final double kP_Theta = 8;
+    public static final double kD_Theta = 0;
+
+    public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
+            new TrapezoidProfile.Constraints(
+                    kMaxRotationRadiansPerSecond, kMaxRotationRadiansPerSecondSquared);
+
+    public static final class Module {
+      public static final double kDriveMotorGearRatio = 7.0;
+      public static final double kTurningMotorGearRatio = 13.333;
+      public static final double kWheelDiameterMeters = Units.inchesToMeters(3.94);
+      public static final int kNeoCPR = 42;
+      public static final int kFalconEncoderCPR = 2048;
+
+      public static final DCMotor kDriveGearbox = DCMotor.getFalcon500(1);
+      public static final DCMotor kTurnGearbox = DCMotor.getNEO(1);
+
+      public static final double kDriveDistancePerPulse =
+              (kWheelDiameterMeters * Math.PI) / (kFalconEncoderCPR * kDriveMotorGearRatio);
+
+      public static final double kDriveRevToMeters =
+              ((kWheelDiameterMeters * Math.PI) / kDriveMotorGearRatio);
+      public static final double kDriveRpmToMetersPerSecond =
+              kDriveRevToMeters / 60.0;
+      public static final double kTurnRotationsToDegrees =
+              360.0 / kTurningMotorGearRatio;
+
+      public static final double ksDriveVoltSecondsPerMeter = 0.667 / 12;
+      public static final double kvDriveVoltSecondsSquaredPerMeter = 2.44 / 12;
+      public static final double kaDriveVoltSecondsSquaredPerMeter = 0.27 / 12;
+
+      public static final double kvTurnVoltSecondsPerRadian = 1.47;
+      public static final double kaTurnVoltSecondsSquaredPerRadian = 0.348;
+    }
+
+    public enum ModulePosition {
+      FRONT_LEFT,
+      FRONT_RIGHT,
+      BACK_LEFT,
+      BACK_RIGHT
+    }
+
+    public static final class Ports {
+      public static final int frontLeftDriveMotor = 1;
+      public static final int frontLeftTurnMotor = 2;
+      public static final int frontRightDriveMotor = 5;
+      public static final int frontRightTurnMotor = 6;
+      public static final int backLeftDriveMotor = 3;
+      public static final int backLeftTurnMotor = 4;
+      public static final int backRightDriveMotor = 7;
+      public static final int backRightTurnMotor = 8;
+
+      public static final int frontLeftCANCoder = 2;
+      public static final int frontRightCANCoder = 8;
+      public static final int backLeftCANCoder = 4;
+      public static final int backRightCANCoder = 6;
+    }
+  }
+
+  // 1 = closed-loop control (using sensor feedback) and 0 = open-loop control (no sensor feedback)
+  public enum CONTROL_MODE {
+    OPENLOOP,
+    CLOSEDLOOP
+  }
+
+public static double kTurnToAngleP;
+public static double kTurnToAngleTolerance;
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,13 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.SwerveDrive;
+import frc.robot.subsystems.SwerveSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,15 +19,31 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+  SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+
+  public static boolean fieldRelativeDriving = true;
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController xbox1 =
+      new CommandXboxController(Constants.OperatorConstants.kDriverControllerPort);
+
+  
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    // Configure default commands
+
+    swerveSubsystem.setDefaultCommand(
+            // The left stick controls translation of the robot.
+            // Turning is controlled by the X axis of the right stick.
+            new SwerveDrive(
+                    swerveSubsystem,
+                    xbox1::getLeftY,
+                    xbox1::getLeftX,
+                    xbox1::getRightX,
+                    xbox1.a(),
+                    fieldRelativeDriving));
     // Configure the trigger bindings
     configureBindings();
   }
@@ -42,13 +58,7 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
   /**
@@ -58,6 +68,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return new WaitCommand(0);
   }
 }
