5e4bed5098a90001eb41d498972dba5399518077
MATTHJA3 <78049146+MATTHJA3@users.noreply.github.com>
2023-03-20 18:11:47 -0500

    Add files via upload

---
+++ src/main/java/frc/robot/subsystems/Trigger.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class Trigger {
+
+    public Trigger RT() {
+        return null;
+    }
+
+    public void whenPressed(Command runIntake) {
+    }
+
+    public void whenReleased(Command stopIntake) {
+    }
+
+}
