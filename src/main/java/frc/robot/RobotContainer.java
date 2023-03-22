5394db48f901854548f3a5a3a5a2a04f2f10baee
Irishtiger876 <89586529+Irishtiger876@users.noreply.github.com>
2023-03-22 11:54:50 -0500

    Add files via upload
    Apriltag targeting with Photonvision
    !Does not move yet!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,22 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import java.util.List;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonTargetSortMode;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.PhotonCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.SwerveDrive;
+import frc.robot.subsystems.SwerveSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,17 +28,63 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+  SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+  Photon photonSubsystem = new Photon();
+  PhotonCommand run = new PhotonCommand(null, null);
+  //Photon photon = new Photon();
+
+  public static boolean fieldRelativeDriving = false;
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController xbox1 =
+      new CommandXboxController(Constants.OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    // Configure default commands
+    PhotonCamera camera = new PhotonCamera("photonvision");
+    
+    PhotonPipelineResult result = camera.getLatestResult();
+    
+   
+            //gets the list of targets seen
+           // List<PhotonTrackedTarget> targets = result.getTargets();
+            //gets the best target
+            PhotonTargetSortMode sortMode = PhotonTargetSortMode.Centermost; 
+            //PhotonPipelineResult result2 = new PhotonPipelineResult();
+            PhotonTrackedTarget target = result.getBestTarget();
+            //PhotonTrackedTarget target1 = result.gets ;
+            //gets the id of the best target
+            int targetID = target.getFiducialId();
+          
+            
+            //outputs the tracked id to smart dashboard
+                SmartDashboard.putNumber("targetID", targetID);
+                System.out.println(targetID);
+                System.out.println("Hi");
+     photonSubsystem.setDefaultCommand(
+        new PhotonCommand(null, xbox1::a);
+        
+     );            
+            
+
+    swerveSubsystem.setDefaultCommand(
+            // The left stick controls translation of the robot.
+            // Turning is controlled by the X axis of the right stick.
+            new SwerveDrive(
+                    swerveSubsystem,
+                    xbox1::getLeftY,
+                    xbox1::getLeftX,
+                    xbox1::getRightX,
+                    fieldRelativeDriving));
+
+            
     // Configure the trigger bindings
-    configureBindings();
+    //configureBindings(
+     
+      
+    
   }
 
   /**
@@ -42,13 +97,7 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
   /**
@@ -58,6 +107,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return new WaitCommand(0);
   }
 }
